e7482d4f3e01bebdc9460f841772a9524d596e7b
Frank100kgbenchpress <piz.frank2@gmail.com>
2024-10-30 21:07:34 -0400

    Teclado Telefonico Exam Solved(is also a leetcode 17)

--- pruebas de recursividad hechas/Balanceando Cadenas/Solution.cs
+++ pruebas de recursividad hechas/Balanceando Cadenas/Solution.cs
@@ -1,52 +1,52 @@
-﻿using System;  
-using System.Collections.Generic;  
+﻿using System;
+using System.Collections.Generic;
 
-namespace Weboo.Examen  
-{  
-    public class CadenasBalanceadas  
-    {  
-        public static int MinOperacionesParaBalancear(string s)  
-        {  
-            Stack<char> stack = new Stack<char>();  
-            int operaciones = 0;  
+namespace Weboo.Examen
+{
+    public class CadenasBalanceadas
+    {
+        public static int MinOperacionesParaBalancear(string s)
+        {
+            var stack = new Stack<char>();
+            int operations = 0;
+            if(s.Length % 2 != 0) return -1;
+            foreach (char c in s)
+            {
+                if (c == '(' || c == '{' || c == '[')
+                {
+                    stack.Push(c); // Almacenar aperturas
+                }
+                else
+                {
+                    if (stack.Count == 0)
+                    {
+                        // Si no hay apertura correspondiente, necesitamos una operación de reemplazo
+                        operations++;
+                    }
+                    else
+                    {
+                        char top = stack.Peek();
+                        if ((c == ')' && top == '(') ||
+                            (c == '}' && top == '{') ||
+                            (c == ']' && top == '['))
+                        {
+                            stack.Pop(); // Par correcto, no se requiere operación adicional
+                        }
+                        else
+                        {
+                            // Desemparejado; incrementar el conteo de operaciones
+                            operations++;
+                            stack.Pop(); // Remover el tope para no causar un desbalance posterior
+                        }
+                    }
+                }
+            }
 
-            // Mapa de correspondencia de caracteres  
-            Dictionary<char, char> pares = new Dictionary<char, char>  
-            {  
-                { '(', ')' },  
-                { '{', '}' },  
-                { '[', ']' }  
-            };  
+            // Cualquier apertura sin cerrar en la pila requiere una operación de cierre
+            operations += stack.Count;
 
-            // Mapa para verificar si un carácter es de apertura  
-            HashSet<char> apertura = new HashSet<char>(pares.Keys);  
-            // Mapa para verificar si un carácter es de cierre  
-            HashSet<char> cierre = new HashSet<char>(pares.Values);  
-
-            foreach (char c in s)  
-            {  
-                if (apertura.Contains(c))  
-                {  
-                    stack.Push(c);  
-                }  
-                else if (cierre.Contains(c))  
-                {  
-                    if (stack.Count > 0 && pares[stack.Peek()] == c)  
-                    {  
-                        stack.Pop(); // Se encuentra un par balanceado  
-                    }  
-                    else  
-                    {  
-                        operaciones++; // Se necesita una operación para balancear  
-                    }  
-                }  
-            }  
-
-            // Al final, los elementos restantes en la pila son los que no se han balanceado  
-            operaciones += stack.Count;  
-
-            // Si hay más operaciones que caracteres, no se puede balancear  
-            return operaciones % 2 == 0 ? operaciones / 2 : -1;  
-        }  
-    }  
+            return operations;
+        }
+    }
 }
+
---
+++ pruebas de recursividad hechas/Teclado Telefonico/Solution.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Extraordinario
+{
+    static Dictionary<char, string> tecladoMap = new Dictionary<char, string>
+    {
+        {'2', "ABC"},
+        {'3', "DEF"},
+        {'4', "GHI"},
+        {'5', "JKL"},
+        {'6', "MNO"},
+        {'7', "PQRS"},
+        {'8', "TUV"},
+        {'9', "WXYZ"}
+    };
+
+    public static IEnumerable<string> Cadenas(string secuenciaTeclas)
+    {
+        List<string> resultados = [];                           //lista a devolver
+        GenerarCombinaciones(secuenciaTeclas, 0, "", resultados);//metodo recursivo que genera combinaciones
+        return resultados;                                      //retornamos la lista
+    }
+
+    static void GenerarCombinaciones(string secuencia, int index, string combinacionActual, List<string> resultados)
+    {
+        if (index == secuencia.Length)  // si llegamos al final de la secuencia
+        {
+            resultados.Add(combinacionActual);  //añadimos a la lista
+            return;                     //salimos
+        }
+
+        char tecla = secuencia[index];  // caracter actual
+        if (tecladoMap.ContainsKey(tecla))  // si lo contiende el diccionario de teclas
+        {
+            foreach (char letra in tecladoMap[tecla])
+            {
+                GenerarCombinaciones(secuencia, index + 1, combinacionActual + letra, resultados);  //por cada tecla llamamos recursivo
+            }
+        }
+        else
+        {
+            GenerarCombinaciones(secuencia, index + 1, combinacionActual, resultados);  //si no saltamos el caracter
+        }
+    }
+}
